cf8978c1210e30891260d9c98bf0e0ce4a393203
Owolabi <owolabi1738@gmail.com>
2025-10-21 14:09:15 +0100

    Updated inline style within heading component. Created a heroes component for images to be on the landing page, and added resources to the public folder

--- notion-clone/app/(marketing)/page.tsx
+++ notion-clone/app/(marketing)/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
 import { Heading } from "./_components/heading"
+import { Heroes } from "./_components/heroes"
 
 export default function MarketingPage() {
   return ( 
     <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-start md:justify-center
+      <div className="flex flex-col items-center justify-center
       text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
+        <Heroes />
       </div>
     </div>
   );
