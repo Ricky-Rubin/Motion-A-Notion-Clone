4b63e192d3368d9c54d7362d76b9b39a0dfd3c69
Owolabi <owolabi1738@gmail.com>
2025-10-21 15:40:31 +0100

    Added two new components to add and style the landing page's footer

---
+++ notion-clone/app/(marketing)/_components/footer.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./logo"
+
+export const Footer = () => {
+    return (
+        <div className="flex items-center w-full bg-background z-50 p-6">
+            <Logo />
+        </div>
+    )
+}
