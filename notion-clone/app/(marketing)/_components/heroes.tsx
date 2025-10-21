eb141ee3f7d6b50a07f9ec5e82aa40cf1c9a79c5
Owolabi <owolabi1738@gmail.com>
2025-10-21 14:32:21 +0100

    Rendered the svg image

--- notion-clone/app/(marketing)/_components/heroes.tsx
+++ notion-clone/app/(marketing)/_components/heroes.tsx
@@ -2,8 +2,17 @@ import Image from "next/image"
 
 export const Heroes = () => {
     return (
-        <div>
-            Heroes
+        <div className="flex flex-col items-center justify-center max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+                    <Image 
+                    src="/team.svg" 
+                    fill
+                    alt="Team-Picture"
+                    className="object-contain"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
