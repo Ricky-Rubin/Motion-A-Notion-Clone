8c71ba23bb4b709c0888558d6a6856fb546a29c7
Owolabi <owolabi1738@gmail.com>
2025-10-22 14:34:52 +0100

    Page scroll function implementation

--- notion-clone/app/(marketing)/_components/navbar.tsx
+++ notion-clone/app/(marketing)/_components/navbar.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useScrollTop } from "@/hooks/use-scroll-top"
+import { cn } from "@/lib/utils"
+
 export const Navbar = () => {
+    const scrolled = useScrollTop();
+
     return (
         <div>
             Navbar!
