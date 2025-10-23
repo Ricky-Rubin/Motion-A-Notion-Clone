4364cbc630383f7ceb79b0e6789d840d23a2336b
Owolabi <owolabi1738@gmail.com>
2025-10-23 16:27:25 +0100

    Implemented dark-mode screen using shadcn

--- notion-clone/app/layout.tsx
+++ notion-clone/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/provider/theme-provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,11 +38,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="motion-theme-2"
+      >
         {children}
+      </ThemeProvider>
       </body>
     </html>
   );
