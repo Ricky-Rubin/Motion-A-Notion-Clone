8189d1d998a746d8f60c8f756d9f7abe61fbc326
Owolabi <owolabi1738@gmail.com>
2025-10-24 11:01:25 +0100

    Updated the size of the logo on the landing page and changed the settings for logo display for light and dark display modes

--- notion-clone/app/(marketing)/_components/logo.tsx
+++ notion-clone/app/(marketing)/_components/logo.tsx
@@ -12,9 +12,18 @@ export const Logo = () => {
         <div className="hidden md:flex items-center gap-x-2">
             <Image 
             src="/logo-dark.svg"
-            width="40"
-            height="40"
+            width="25"
+            height="25"
             alt="Logo"
+            className="dark:hidden"
+            />
+
+            <Image 
+            src="/logo.svg"
+            width="25"
+            height="25"
+            alt="Logo"
+            className="hidden dark:block"
             />
 
             <p className={cn("font-semibold", font.className)}>
