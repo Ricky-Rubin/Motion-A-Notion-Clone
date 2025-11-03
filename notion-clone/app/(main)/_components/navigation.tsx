8bd1b40d8447f669bf8e03a854a06e81e7e6126f
Owolabi <owolabi1738@gmail.com>
2025-11-03 15:04:29 +0100

    Wrote the function to reset the width of the navbar to default when the right border is clicked

--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -49,6 +49,24 @@ export const Navigation = () => {
         document.removeEventListener("mouseup", handleMouseUp);
     };
 
+    const resetWidth = () => {
+        if (sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(false);
+            setIsResetting(true);
+
+            sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+            navbarRef.current.style.setProperty(
+                "width",
+                isMobile ? "0" : "calc(100% -240px)"
+            );
+            navbarRef.current.style.setProperty(
+                "left",
+                isMobile ? "100%" : "240px"
+            );
+            setTimeout(() => setIsResetting(false), 300);
+        }
+    }
+
     return (
         <>
             <aside
@@ -79,7 +97,7 @@ export const Navigation = () => {
                 
                 <div
                     onMouseDown={handleMouseDown}
-                    onClick={() => {}}
+                    onClick={() => {resetWidth}}
                     className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full
                      w-1 bg-primary/10 right-0 top-0"
                 />
