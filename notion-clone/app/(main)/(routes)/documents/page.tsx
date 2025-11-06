44631a51f4ce57386f6e2cd278e6df5f2a9ee507
Owolabi <owolabi1738@gmail.com>
2025-11-06 16:11:43 +0100

    Added the static animation for the documents page/internal page to be accessed after logging into your Motion account

--- notion-clone/app/(main)/(routes)/documents/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,25 @@
+"use client"
+
+import Image from "next/image";
+
 const DocumentsPage = () => {
     return (
-        <div>
-            This is a protected page!
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image 
+                src="/empty.svg"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="dark:hidden"
+            />
+
+            <Image 
+                src="/empty-dark.svg"
+                height="300"
+                width="300"
+                alt="Empty-Dark"
+                className="hidden dark:block"
+            />
         </div>
     );
 };
