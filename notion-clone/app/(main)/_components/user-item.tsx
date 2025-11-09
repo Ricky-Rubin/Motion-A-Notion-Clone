9a6da98d7accbf4ba4948da38079154ec227c45c
Owolabi <owolabi1738@gmail.com>
2025-11-09 13:54:40 +0100

    Completed the user information portion of the sidebar

--- notion-clone/app/(main)/_components/user-item.tsx
+++ notion-clone/app/(main)/_components/user-item.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useUser } from "@clerk/clerk-react"
+import { useUser, SignOutButton } from "@clerk/clerk-react"
 
 import { ChevronsLeftRight } from "lucide-react"
 
@@ -23,8 +23,8 @@ export const UserItem = () => {
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
-                <div role="button" className="flex items-center text-sm p-3 w-full hover:bg-primary">
+            <DropdownMenuTrigger asChild>
+                <div role="button" className="flex items-center text-sm p-3 w-full hover:bg-primary/5">
                     <div className="gap-x-2 flex items-center max-w-[150px]">
                         <Avatar className="h-5 w-5">
                             <AvatarImage src={user?.imageUrl} />
@@ -33,8 +33,47 @@ export const UserItem = () => {
                             {user?.fullName}&apos;s Motion
                         </span>
                     </div>
+
+                    <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
                 </div>
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+                className="w-80"
+                align="start"
+                alignOffset={11}
+                forceMount
+            >
+                <div className="flex flex-col space-y-4 p-2">
+                    <p className="text-xs font-medium leading-none text-muted-foreground">
+                        {user?.emailAddresses[0].emailAddress}
+                    </p>
+
+                    <div className="flex items-center gap-x-2">
+                        <div className="rounded-md bg-secondary p-1">
+                            <Avatar className="h-8 w-8">
+                                <AvatarImage src={user?.imageUrl} />
+                            </Avatar>
+                        </div>
+
+                        <div className="space-y-1">
+                            <p className="text-sm line-clamp-1">
+                                {user?.fullName}&apos;s Motion
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem asChild className="w-full cursor-pointer text-muted-foreground">
+                    <SignOutButton>
+                        Log out
+                    </SignOutButton>
+                </DropdownMenuItem>
+        
+            </DropdownMenuContent>
+
         </DropdownMenu>
     )
 }
