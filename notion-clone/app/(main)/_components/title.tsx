4ed493107cb2058ed1e9bf146b8132a53ebb667a
Owolabi <owolabi1738@gmail.com>
2025-11-22 12:49:56 +0100

    Editing document title with updates happening in realtime implemented

--- notion-clone/app/(main)/_components/title.tsx
+++ notion-clone/app/(main)/_components/title.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
@@ -16,20 +16,60 @@ export const Title = ({
     initialData
 }: TitleProps) => {
 
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [title, setTitle] = useState(initialData.title || "Unititled");
+
     const update = useMutation(api.documents.update);
     const [isEditing, setIsEditing] = useState(false);
+
+    const enableInput = () => {
+        setTitle(initialData.title);
+        setIsEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(0, inputRef.current.value.length)
+        }, 0);
+    }
+
+    const disableInput = () => {
+        setIsEditing(false);
+    };
+
+    const onChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTitle(event.target.value);
+        update({
+            id: initialData._id,
+            title: event.target.value || "Untitled"
+        });
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if (event.key === "Enter") {
+            disableInput();
+        }
+    };
 
     return (
         <div className="flex items-center gap-x-1">
             {!!initialData.icon && <p>{initialData.icon}</p>}
 
             {isEditing ? (
-                <Input 
+                <Input
+                    ref={inputRef}
+                    onClick={enableInput}
+                    onBlur={disableInput}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={title}
                     className="h-7 px-2 focus-visible:ring-transparent"
                 />
             ) : (
                 <Button 
-                    onClick={() => {}}
+                    onClick={enableInput}
                     variant="ghost"
                     size="sm"
                     className="font-normal h-auto p-1"
