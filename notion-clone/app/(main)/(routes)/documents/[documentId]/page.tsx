40d2af4515958915ab342ab05defa66ab54ac4cf
Owolabi <owolabi1738@gmail.com>
2025-11-27 10:55:45 +0100

    Removed the extra exclamation turning initialData.icon into a boolean and got the code fixed

--- notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,49 @@
-const DocumentIdPage = () => {
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { Toolbar } from "@/components/toolbar";
+import { use } from "react";
+
+interface DocumentIdPageProps {
+    params: Promise<{
+        documentId: Id<"documents">;
+    }>;
+};
+
+const DocumentIdPage = ({
+    params
+}: DocumentIdPageProps) => {
+
+    const { documentId } = use(params);
+
+    const document = useQuery(api.documents.getById, {
+        documentId: documentId
+    });
+
+    if (document === undefined) {
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    };
+
+    if (document === null) {
+        return <div>Not found</div>
+    }
+
     return (
-        <div>
-            DocumentId
+        <div className="pb-40">
+            <div className="h-[35vh]" />
+
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document} />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default DocumentIdPage;
--- notion-clone/app/(main)/_components/navbar.tsx
+++ notion-clone/app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { MenuIcon } from "lucide-react";
 
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -29,8 +30,12 @@ export const Navbar = ({
     if (document === undefined) {
         return (
             <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full
-            flex items-center">
+            flex items-center justify-between">
                 <Title.Skeleton />
+
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -55,6 +60,10 @@ export const Navbar = ({
                     <Title 
                         initialData={document}
                     />
+
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
 
