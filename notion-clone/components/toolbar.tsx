5d1e8550a3521a5a93bd2caf12f77debaf87db2a
Owolabi <owolabi1738@gmail.com>
2025-11-27 16:32:38 +0100

    Implemented the logic to have each document title show on the main input area and make it modifiable from there as well

--- notion-clone/components/toolbar.tsx
+++ notion-clone/components/toolbar.tsx
@@ -5,6 +5,12 @@ import { IconPicker } from "./icon-picker"
 import { X, Smile, ImageIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { useRef, ElementRef, useState } from "react";
+import { useMutation } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
+import TextareaAutosize from "react-textarea-autosize";
 
 interface ToolbarProps {
     initialData: Doc<"documents">;
@@ -15,6 +21,44 @@ export const Toolbar = ({
     initialData,
     preview
 }: ToolbarProps) => {
+
+    const inputRef = useRef<ElementRef<"textarea">>(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(initialData.title);
+    
+    const update = useMutation(api.documents.update);
+
+    const enableInput = () => {
+        if (preview) return;
+
+        setIsEditing(true);
+        setTimeout(() => {
+            setValue(initialData.title);
+            inputRef.current?.focus();
+        }, 0);
+    };
+
+    const disableInput = () => {
+        setIsEditing(false);
+    };
+
+    const onInput = (value: string) => {
+        setValue(value);
+        update({
+            id: initialData._id,
+            title: value || "Untitled"
+        });
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLTextAreaElement>
+    ) => {
+        if (event.key ==="Enter") {
+            event.preventDefault();
+            disableInput();
+        }
+    };
+
     return (
         <div className="pl-[54px] group relative">
             {!!initialData.icon && !preview && (
@@ -29,7 +73,7 @@ export const Toolbar = ({
                         onClick={() => {}}
                         className="rounded-full opacity-0 group-hover/icon:opacity-100
                         transition text-muted-foreground text-xs"
-                        variant="outline"
+                         variant="outline"
                         size="icon"
                     >
                         <X className='h-4 w-4' />
@@ -66,9 +110,30 @@ export const Toolbar = ({
                         size="sm"
                     >
                         <ImageIcon className="h-4 w-4 mr-2" />
+                        Add cover
                     </Button>
                 )}
             </div>
+
+            {isEditing && !preview ? (
+                <TextareaAutosize 
+                    ref={inputRef}
+                    onBlur={disableInput}
+                    onKeyDown={onKeyDown}
+                    value={value}
+                    onChange={(e) => onInput(e.target.value)}
+                    className="text-5xl bg-transparent font-bold break-words
+                    outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+                />
+            ) : (
+                <div
+                    onClick={enableInput}
+                    className="pb-[11.5px] text-5xl font-bold break-words
+                    outline-none text-[#3F3F3F] dark:text-[#CFCFCF]" 
+                >
+                    {initialData.title}
+                </div>
+            )}
         </div>
     )
 }
