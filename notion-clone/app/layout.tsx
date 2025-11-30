a299d85905b2eecba76aeda2713f9e2792207f9b
Owolabi <owolabi1738@gmail.com>
2025-11-30 18:39:25 +0100

    Installed edgestore and modified some lines within the codebase to make image upload possible

--- notion-clone/app/layout.tsx
+++ notion-clone/app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "sonner";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 import "./globals.css";
 
 import { ConvexClientProvider } from "@/components/ui/provider/convex-provider";
@@ -48,17 +49,19 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <ConvexClientProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="motion-theme-2"
-        >
-          <Toaster position="bottom-center" />
-          <ModalProvider />
-          {children}
-        </ThemeProvider>
+        <EdgeStoreProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="motion-theme-2"
+          >
+            <Toaster position="bottom-center" />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </EdgeStoreProvider>
       </ConvexClientProvider>
       </body>
     </html>
--- notion-clone/components/ui/modals/cover-image-modal.tsx
+++ notion-clone/components/ui/modals/cover-image-modal.tsx
@@ -7,10 +7,51 @@ import {
     DialogTitle
 } from "@/components/ui/dialog"
 
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { SingleImageDropzone } from "@/components/upload/single-image";
 
 export const CoverImageModal= () => {
+
+    const update = useMutation(api.documents.update);
+    const params = useParams();
+
+    const [file, setFile] = useState<File>();
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
     const coverImage = useCoverImage();
+    const { edgestore } = useEdgeStore();
+
+    const onClose = () =>{
+        setFile(undefined);
+        setIsSubmitting(false);
+        coverImage.onClose();
+    }
+
+    const onChange = async (file?: File) => {
+        if (file) {
+            setIsSubmitting(true);
+            setFile(file);
+
+            const res = await edgestore.publicFiles.upload({
+                file
+            });
+
+            await update({
+                id: params.documentId as Id<"documents">,
+                coverImage: res.url
+            });
+
+            onClose();
+        }
+    }
 
     return (
         <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
@@ -21,9 +62,9 @@ export const CoverImageModal= () => {
                     </DialogTitle>
                 </DialogHeader>
 
-                <div>
-                    TODO: Upload image
-                </div>
+                <SingleImageDropzone 
+                    className="w-full outline-none"
+                />
             </DialogContent>
         </Dialog>
     )
