80afbbf60abdd0145a52ca29bce42f95833ab94c
Owolabi <owolabi1738@gmail.com>
2026-01-06 13:53:20 +0100

    Fix Clerk auth domain in Convex config

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://vital-goldfish-95.clerk.accounts.dev",
+            domain: "vital-goldfish-95.clerk.accounts.dev",
             applicationID: "convex",
         }
     ]
